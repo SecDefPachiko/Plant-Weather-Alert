18eb8b24cc1ad56d9d6166a1e2f6a61cc538f322
Patrick Delgado <delgadopat93@gmail.com>
2024-03-13 13:25:52 -0600

    Changed url for API request. Converted units to imperial (farenheit). Changes to reference proper JSON index.

--- PlantWarning.py
+++ PlantWarning.py
@@ -1,25 +1,24 @@
 import requests
 
 def get_location(api_key, zip_code, country_code):
-    url = f'https://pro.openweathermap.org/data/2.5/forecast/hourly?zip={zip_code},{country_code}&appid={api_key}'
+    url = f'http://api.openweathermap.org/geo/1.0/zip?zip={zip_code},{country_code}&appid={api_key}'
     response = requests.get(url)
     data = response.json()
     return data
 
 def get_weather(api_key, lat, lon):
-    url = f'https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={api_key}'
+    url = f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}&units=imperial'
     response = requests.get(url)
     data = response.json()
     return data
 
 def notify_user(weather_data):
-    temperature = weather_data['main']['temp']
-    weather_condition = weather_data['weather'][0]['main']
-
-    if temperature < 5:
+    temperature = float(weather_data['list'][0]['main']['temp'])
+    weather_condition = weather_data['list'][0]['weather'][0]['main']
+    if temperature < 32:
         print("Warning: It's very cold outside. Bring sensitive plants indoors.")
-    elif weather_condition in ['Rain', 'Thunderstorm', 'Snow']:
-        print("Warning: Inclement weather expected. Bring outdoor plants indoors.")
+    elif ['Rain', 'Thunderstorm', 'Snow'] in weather_condition:
+        print("Warning: Inclement weather expected. Bring outdoor plants under porch.")
     else:
         print("Weather conditions are favorable for outdoor plants.")
 
@@ -31,10 +30,10 @@ def main():
     location_data = get_location(api_key, zip_code, country_code)
     lat = location_data['lat']
     lon = location_data['lon']
-    #lat and long of location from get_location used in get_weather function to request weather data
+    # #lat and long of location from get_location used in get_weather function to request weather data
     weather_data = get_weather(api_key, lat, lon)
-    #ensures that 'main' and 'weather is present inside the json returned from get_weather function
-    if 'main' in weather_data and 'weather' in weather_data:
+    # ensures that 'main' and 'weather is present inside the json returned from get_weather function
+    if 'main' in weather_data['list'][0] and 'weather' in weather_data['list'][0]:
         notify_user(weather_data)
     else:
         print("Failed to fetch weather data. Please check your city name and API key.")
